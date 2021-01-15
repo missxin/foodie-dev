901d59b15d2890dc59444eabbd4301bc8fe2a14a
Bigoldcow <757411465@qq.com>
2021-01-15 22:12:41 +0800

    数据源整合

--- foodie-api/src/main/java/com/imooc/controller/HelloController.java
+++ foodie-api/src/main/java/com/imooc/controller/HelloController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    @GetMapping("/Hello")
+    @GetMapping("/hello")
     public Object hello(){
         return "Hello World";
     }
